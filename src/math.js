135ebcb337a32fc7c6e386b7af614748374476cb
nataliehippe <natalie.alibegashvili@gmail.com>
2021-06-03 12:01:11 +0400

    add async await tests

--- src/math.js
+++ src/math.js
@@ -5,9 +5,17 @@ const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8;
 
 const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32;
 
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+}
 
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,22 +1,33 @@
-const {calculateTip,fahrenheitToCelsius,celsiusToFahrenheit}=require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math');
 
 
-test('Should calculate total with tip',()=>{
+test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3);
     expect(total).toBe(13);
-}); 
+});
 
-test('Should calculate total with default tip',()=>{
+test('Should calculate total with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
 });
 
-test('Should convert 32 F to 0 C',()=>{
-    const temperature=fahrenheitToCelsius(32);
+test('Should convert 32 F to 0 C', () => {
+    const temperature = fahrenheitToCelsius(32);
     expect(temperature).toBe(0);
 });
 
-test('Should convert 0 C to 32 F',()=>{
-    const temperature=celsiusToFahrenheit(0);
+test('Should convert 0 C to 32 F', () => {
+    const temperature = celsiusToFahrenheit(0);
     expect(temperature).toBe(32);
+});
+
+test('Should add two number', () => {
+    add(4, 5).then((sum) => {
+        expect(sum).toBe(9);
+    });
+});
+
+test('Should add two number async/await', async () => {
+    const sum = await add(34, 1);
+    expect(sum).toBe(35);
 });
