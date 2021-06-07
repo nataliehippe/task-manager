fc0d36389b37a91e98c2fd948592ae7e3c5e4387
nataliehippe <natalie.alibegashvili@gmail.com>
2021-06-07 16:04:16 +0400

    Add test case for delete user authorized/unauthorized

--- tests/user.test.js
+++ tests/user.test.js
@@ -53,9 +53,24 @@ test('Should get profile for user', async () => {
         .expect(200);
 });
 
-test('Should not get profile for unauthenticated user',async()=>{
+test('Should not get profile for unauthenticated user', async () => {
     await request(app)
         .get('/users/me')
+        .send()
+        .expect(401)
+});
+
+test('Should delete account for user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization','Bearer '+userOne.tokens[0].token)
+    .send()
+    .expect(200)
+});
+
+test('Should not delete account for unauthenticated user',async()=>{
+    await request(app)
+        .delete('/users/me')
         .send()
         .expect(401)
 });
